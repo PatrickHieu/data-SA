c3687615da8b74533a0373ca0fc5a586cc98cfc0
Patrick_Hieu <hieunguyenthanh204@gmail.com>
2023-11-03 10:15:53 +0700

    add two interger

--- add_two_intergers.c
+++ add_two_intergers.c
@@ -9,7 +9,7 @@ int main() {
     int length_b = strlen(b);
     int length_sum;
     // so sánh độ dài xâu a và b để xác định độ dài xâu sum
-    int  t = 0;
+    length_sum = (length_a > length_b) ? length_a + 1 : length_b + 1;
     
     // biến tổng dùng để tính tổng hai chữ số trong mỗi vòng lặp, biến nhớ dùng để lưu trữ phần dư(nếu tổng > 10)
     int tong , so_nho = 0;
